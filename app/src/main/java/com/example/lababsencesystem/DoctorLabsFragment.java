6f1f1640f52201c88c1bc0ce4d73aa20e35b1a17
hussein barakat <husseinbarakat678@gmail.com>
2020-04-22 19:47:38 +0300

    Reading excel not finished

--- app/src/main/java/com/example/lababsencesystem/DoctorLabsFragment.java
+++ app/src/main/java/com/example/lababsencesystem/DoctorLabsFragment.java
@@ -22,7 +22,7 @@ public class DoctorLabsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        // Inflate the layout for this fragmen t
         return inflater.inflate(R.layout.fragment_doctor_labs, container, false);
     }
 }
