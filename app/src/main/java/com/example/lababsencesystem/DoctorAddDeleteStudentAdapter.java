18551d5ec4f02c466d48a630dae18c31285ffeac
hussein barakat <husseinbarakat678@gmail.com>
2020-04-28 00:24:17 +0300

    DoctorCourse Next Button

--- app/src/main/java/com/example/lababsencesystem/DoctorAddDeleteStudentAdapter.java
+++ app/src/main/java/com/example/lababsencesystem/DoctorAddDeleteStudentAdapter.java
@@ -5,18 +5,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class DoctorAddDeleteStudentAdapter extends RecyclerView.Adapter<DoctorAddDeleteStudentAdapter.DoctorAddDeleteStudentViewHolder> {
 
     ArrayList<Student> students;
+    ArrayList<CourseStudent> cs;
+    int flag,w;
+    String getCourse;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-    public DoctorAddDeleteStudentAdapter(ArrayList<Student> students) {
+    public DoctorAddDeleteStudentAdapter(ArrayList<Student> students,ArrayList<CourseStudent> cs,int flag,String getCourse) {
             this.students=students;
+            this.cs=cs;
+            this.flag=flag;
+            this.getCourse=getCourse;
     }
 
     @NonNull
@@ -27,14 +39,67 @@ public class DoctorAddDeleteStudentAdapter extends RecyclerView.Adapter<DoctorAd
         return dd;    }
 
     @Override
-    public void onBindViewHolder(@NonNull DoctorAddDeleteStudentAdapter.DoctorAddDeleteStudentViewHolder holder, int position) {
-        holder.filenbb.setText(students.get(position).getFileNumber()+"");
-        holder.namee.setText(students.get(position).getName());
+    public void onBindViewHolder(@NonNull final DoctorAddDeleteStudentAdapter.DoctorAddDeleteStudentViewHolder holder, final int position) {
+
+        if(flag==1) {
+            holder.filenbb.setText(cs.get(position).getFileNumber() + "");
+            holder.namee.setText(cs.get(position).getName());
+            holder.deletee.setVisibility(View.VISIBLE);
+        }
+        else if (flag==0){
+            holder.addd.setVisibility(View.VISIBLE);
+            holder.namee.setText(students.get(position).getName());
+            holder.filenbb.setText(students.get(position).getFileNumber() + "");
+        }
+        else{
+           if (position<cs.size()) {
+                holder.deletee.setVisibility(View.VISIBLE);
+                holder.namee.setText(cs.get(position).getName());
+                holder.filenbb.setText(cs.get(position).getFileNumber() + "");
+            }
+            else{
+                holder.addd.setVisibility(View.VISIBLE);
+                holder.namee.setText(students.get(position-cs.size()).getName());
+                holder.filenbb.setText(students.get(position-cs.size()).getFileNumber() + "");
+            }
+        }
+
+        holder.addd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (flag==0) {
+                    CourseStudent courseStudent = new CourseStudent(students.get(position).getName(), students.get(position).getFileNumber());
+                    db.collection("courses").document(getCourse).collection("students").document(courseStudent.getFileNumber() + "").set(courseStudent);
+                    removest(position,-1);
+                }
+                else {
+                    CourseStudent courseStudent = new CourseStudent(students.get(position-cs.size()).getName(), students.get(position-cs.size()).getFileNumber());
+                    db.collection("courses").document(getCourse).collection("students").document(courseStudent.getFileNumber() + "").set(courseStudent);
+
+                    removest(position,cs.size());
+                }
+            }
+        });
+
+        holder.deletee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CourseStudent courseStudent = new CourseStudent(cs.get(position).getName(), cs.get(position).getFileNumber());
+                db.collection("courses").document(getCourse).collection("students").document(courseStudent.getFileNumber() + "").delete();
+                removecs(position);
+            }
+        });
+
     }
 
     @Override
     public int getItemCount() {
-        return students.size();
+        if (flag==1)
+            return cs.size();
+        else if (flag==0)
+            return students.size();
+        else
+            return cs.size()+students.size();
     }
 
     public static class DoctorAddDeleteStudentViewHolder extends RecyclerView.ViewHolder {
@@ -48,8 +113,23 @@ public class DoctorAddDeleteStudentAdapter extends RecyclerView.Adapter<DoctorAd
             namee=itemView.findViewById(R.id.namee);
             addd=itemView.findViewById(R.id.Addd);
             deletee=itemView.findViewById(R.id.deletee);
-            filenbb.setText("id");
-            namee.setText("id");
+            deletee.setVisibility(View.GONE);
+            addd.setVisibility(View.GONE);
+        }
+    }
+    public void removecs(int position) {
+        cs.remove(position);
+        notifyDataSetChanged();
+    }
+
+    public void removest(int position,int y) {
+        if (y==-1) {
+            students.remove(position);
+            notifyDataSetChanged();
+        }
+        else {
+            students.remove(position - y);
+            notifyDataSetChanged();
         }
     }
 }
--- app/src/main/java/com/example/lababsencesystem/EditCourseStudentsFragment.java
+++ app/src/main/java/com/example/lababsencesystem/EditCourseStudentsFragment.java
@@ -23,9 +23,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -41,7 +44,7 @@ public class EditCourseStudentsFragment extends Fragment {
 
 
     ProgressBar progressBar;
-    TextView textImport,textShow;
+    TextView textImport,textShow,eror;
     EditText enterStudentId;
     Button chooseFile,search,submit;
     Intent myFileIntent;
@@ -50,6 +53,7 @@ public class EditCourseStudentsFragment extends Fragment {
     Workbook workbook;
     ArrayList<Student> students;
     ArrayList<CourseStudent> cs;
+    int flag=0;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public EditCourseStudentsFragment() {
@@ -72,6 +76,8 @@ public class EditCourseStudentsFragment extends Fragment {
         submit.setVisibility(View.GONE);
         progressBar=view.findViewById(R.id.prbar);
         progressBar.setVisibility(View.GONE);
+        eror=view.findViewById(R.id.eror);
+        eror.setVisibility(View.GONE);
 
         name = new ArrayList<>();
         id = new ArrayList<>();
@@ -79,63 +85,152 @@ public class EditCourseStudentsFragment extends Fragment {
         students=new ArrayList<>();
         cs=new ArrayList<>();
 
-       // students.add(new Student("Aa","sa@s.com","321233","123445",18,"Sdd"));
+        String getCourseCode="";
+        Intent intent=getActivity().getIntent();
+        getCourseCode=intent.getStringExtra("CourseCode");
+        textShow.setText(getCourseCode);
+
 
         final RecyclerView rv = view.findViewById(R.id.addDeleteRv);
         rv.setHasFixedSize(true);
         RecyclerView.LayoutManager lm = new LinearLayoutManager(getContext());
         rv.setLayoutManager(lm);
 
+        final String finalGetCourseCode = getCourseCode;
+
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 students.clear();
-
-                String s=enterStudentId.getText().toString();
-                if (!s.equals("")){
+                cs.clear();
+                eror.setVisibility(View.GONE);
+                final String s=enterStudentId.getText().toString();
+                if (!s.equals("")) {
+                    search.setEnabled(false);
                     progressBar.setVisibility(View.VISIBLE);
-                    db.collection("users").document("students").collection("data").document(s).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                            if (task.isSuccessful()){
-                                String name=task.getResult().getString("name");
-                                String username=task.getResult().getString("username");
-                                String password=task.getResult().getString("password");
-                                String email=task.getResult().getString("email");
-                                String type=task.getResult().getString("type");
-                                int filenb=task.getResult().getLong("fileNumber").intValue();
+
+                    final String[] strings = s.split("-");
+
+                    if (strings.length == 2) {
+                        flag=2;
+                        db.collection("courses").document(finalGetCourseCode).collection("students").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()){
+
+                                    if (task.getResult().size() > 0)
+                                        for (QueryDocumentSnapshot document : task.getResult()){
+                                            if (document.getLong("fileNumber").intValue() >= Integer.parseInt(strings[0])
+                                                    && document.getLong("fileNumber").intValue() <= Integer.parseInt(strings[1])){
+                                                String name = document.getString("name");
+
+                                                int filenb = document.getLong("fileNumber").intValue();
+                                                CourseStudent ss = new CourseStudent(name, filenb);
+                                                cs.add(ss);
+                                            }
+
+                                        }
+
+                                    db.collection("users").document("students").collection("data").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                int ln=cs.size();
+                                                if (task.getResult().size() > 0)
+                                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                                        if (document.getLong("fileNumber").intValue() >= Integer.parseInt(strings[0])
+                                                                && document.getLong("fileNumber").intValue() <= Integer.parseInt(strings[1])) {
+                                                            String name = document.getString("name");
+                                                            String username = document.getString("username");
+                                                            String password = document.getString("password");
+                                                            String email = document.getString("email");
+                                                            String type = document.getString("type");
+                                                            int filenb = document.getLong("fileNumber").intValue();
+
+                                                            Student st = new Student(name, email, username, password, filenb, type);
+                                                            students.add(st);
 
 
-                                Student student=new Student(name,email,username,password,filenb,type);
-                                students.add(student);
-
-                                try {
-                                    Thread.sleep(3000);
-                                    RecyclerView.Adapter a = new DoctorAddDeleteStudentAdapter(students);
-                                    rv.setAdapter(a);
-                                    progressBar.setVisibility(View.GONE);
-                                    Log.d("asas",students.size()+"");
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
+                                                        }
+                                                    }
+                                            }
+                                            try {
+                                                Thread.sleep(1000);
+                                                for (int l=0;l<students.size();l++){
+                                                    for (int o=0;o<cs.size();o++){
+                                                        if (cs.get(o).getFileNumber() == students.get(l).getFileNumber())
+                                                            students.remove(l);
+                                                    }
+                                                }
+                                                search.setEnabled(true);
+                                                RecyclerView.Adapter a = new DoctorAddDeleteStudentAdapter(students, cs, flag,finalGetCourseCode);
+                                                rv.setAdapter(a);
+                                                progressBar.setVisibility(View.GONE);
+                                            } catch (InterruptedException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    });
                                 }
 
                             }
+                        });
 
-                        }
-                    });
+                    } else{
+
+                        db.collection("courses").document(finalGetCourseCode).collection("students").document(s).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    if (task.getResult().exists()) {
+                                        String name = task.getResult().getString("name");
+                                        int filenb = task.getResult().getLong("fileNumber").intValue();
+
+                                        CourseStudent ss = new CourseStudent(name, filenb);
+                                        cs.add(ss);
+                                        flag = 1;
+
+                                    } else {
+                                        db.collection("users").document("students").collection("data").document(s).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    if (task.getResult().exists()) {
+                                                        String name = task.getResult().getString("name");
+                                                        String username = task.getResult().getString("username");
+                                                        String password = task.getResult().getString("password");
+                                                        String email = task.getResult().getString("email");
+                                                        String type = task.getResult().getString("type");
+                                                        int filenb = task.getResult().getLong("fileNumber").intValue();
+
+                                                        Student st = new Student(name, email, username, password, filenb, type);
+                                                        students.add(st);
+                                                        flag = 0;
+                                                    }
+                                                }
+                                            }
+                                        });
+                                        //eror.setText("File Number doesn't exists");
+                                        //eror.setVisibility(View.VISIBLE);
+                                    }
+
+                                    try {
+                                        Thread.sleep(1000);
+                                        search.setEnabled(true);
+                                        RecyclerView.Adapter a = new DoctorAddDeleteStudentAdapter(students, cs, flag,finalGetCourseCode);
+                                        rv.setAdapter(a);
+                                        progressBar.setVisibility(View.GONE);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        });
+                }
                 }
 
             }
         });
-
-
-
-
-
-        String getCourseCode="";
-        Intent intent=getActivity().getIntent();
-        getCourseCode=intent.getStringExtra("CourseCode");
-        textShow.setText(getCourseCode);
 
         chooseFile.setOnClickListener(new View.OnClickListener() {
             @Override
