72cfc31bde624b1ef30350ef3a4ee73458b1d84a
hussein barakat <husseinbarakat678@gmail.com>
2020-04-29 03:58:56 +0300

    Merge branch 'master' of C:\Users\Hussein\AndroidStudioProjects\LabAbsence with conflicts.

--- app/src/main/java/com/example/lababsencesystem/DoctorProfileFragment.java
+++ app/src/main/java/com/example/lababsencesystem/DoctorProfileFragment.java
@@ -1,27 +1,24 @@
 package com.example.lababsencesystem;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.Map;
 
 
 /**
@@ -31,8 +28,10 @@ public class DoctorProfileFragment extends Fragment {
 
     Doctor doctor = DoctorMain.doctor;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    EditText nm, un, em, pass, fnb;
-    Button ed;
+    EditText nm, un, em, pass, fnb,oldPassword,newPassword,newPasswordConfirm;
+    TextView usernmaeTv,fileNumberbTv,emailTv,nameTv;
+    Button ed,changePassword;
+    LinearLayout linearEdit,linearShow,linearChangePassword;
 
     public DoctorProfileFragment() {
         // Required empty public constructor
@@ -52,25 +51,136 @@ public class DoctorProfileFragment extends Fragment {
         nm = view.findViewById(R.id.doctorProfilename);
         un = view.findViewById(R.id.doctorProfileUsername);
         em = view.findViewById(R.id.doctorProfileEmail);
-        pass = view.findViewById(R.id.doctorProfilePassword);
-        fnb = view.findViewById(R.id.doctorProfileFileNumber);
+        //pass = view.findViewById(R.id.doctorProfilePassword);
+        //fnb = view.findViewById(R.id.doctorProfileFileNumber);
         ed = view.findViewById(R.id.editDoctorProfile);
+        changePassword=view.findViewById(R.id.changePassword);
 
-        nm.setText(doctor.getName());
-        un.setText(doctor.getUsername());
-        em.setText(doctor.getEmail());
-        pass.setText(doctor.getPassword());
-        fnb.setText(String.valueOf(doctor.getFileNumber()));
-        Log.d("doctorcheck",doctor.toString());
+        usernmaeTv=view.findViewById(R.id.doctorProfileUsernametv);
+        emailTv=view.findViewById(R.id.doctorProfileEmailtv);
+        fileNumberbTv=view.findViewById(R.id.doctorProfileFileNumberTv);
+        nameTv=view.findViewById(R.id.doctorProfilenametv);
+
+        oldPassword=view.findViewById(R.id.oldPassword);
+        newPassword=view.findViewById(R.id.newPassword);
+        newPasswordConfirm=view.findViewById(R.id.newPasswordConfirm);
+
+        linearChangePassword=view.findViewById(R.id.linearChangePassword);
+        linearEdit=view.findViewById(R.id.linearEdit);
+        linearShow=view.findViewById(R.id.linearShow);
+
+        linearShow.setVisibility(View.VISIBLE);
+        linearEdit.setVisibility(View.GONE);
+        linearChangePassword.setVisibility(View.GONE);
+
+
+
+        refresh();
+        //pass.setText(doctor.getPassword());
+        //fnb.setText(String.valueOf(doctor.getFileNumber()));
+
         ed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DoctorMain.doctor.setEmail(em.getText().toString());
-                DoctorMain.doctor.setName(nm.getText().toString());
-                DoctorMain.doctor.setPassword(pass.getText().toString());
-                DoctorMain.doctor.setUsername(un.getText().toString());
-                edit();
-                Toast.makeText(getContext(),"Your Information had been edited",Toast.LENGTH_LONG).show();
+                String getEditPosition=ed.getText().toString();
+                if (getEditPosition.equalsIgnoreCase("Submit")){
+                    int flag=0;
+                    if (nm.getText().toString().equals("") ){
+                        nm.setError("please enter your name");
+                        flag=1;
+                    }
+                    if(em.getText().toString().equals("")) {
+                        em.setError("please enter your email");
+                        flag=1;
+                    }
+                    if(un.getText().toString().equals("")) {
+                        un.setError("please enter your username");
+                        flag=1;
+                    }
+                    if (flag==0) {
+                        ed.setText("Edit");
+                        ed.setBackgroundColor(Color.parseColor("#FF3F51B5"));
+                        linearShow.setVisibility(View.VISIBLE);
+                        linearEdit.setVisibility(View.GONE);
+                        changePassword.setVisibility(View.VISIBLE);
+                        DoctorMain.doctor.setEmail(em.getText().toString());
+                        DoctorMain.doctor.setName(nm.getText().toString());
+                        DoctorMain.doctor.setUsername(un.getText().toString());
+                        edit();
+                        refresh();
+                    }
+
+                }
+                else {
+                    ed.setText("Submit");
+                    ed.setBackgroundColor(Color.parseColor("#FF4CAF50"));
+                    linearEdit.setVisibility(View.VISIBLE);
+                    linearShow.setVisibility(View.GONE);
+                    changePassword.setVisibility(View.GONE);
+                }
+
+            }
+        });
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getPasswordPosition=changePassword.getText().toString();
+                if (getPasswordPosition.equalsIgnoreCase("Submit")){
+                    int flag=0;
+
+                    if (!oldPassword.getText().toString().equals(doctor.getPassword())) {
+                        oldPassword.setError("Incorrect Password");
+                        flag = 1;
+                    }
+                    if (!newPassword.getText().toString().equals(newPasswordConfirm.getText().toString())){
+                        Toast.makeText(getActivity(),"new Password and Confirm new Password fields are different",Toast.LENGTH_LONG).show();
+                        flag=1;
+                    }
+                    if (oldPassword.getText().length()<6) {
+                        oldPassword.setError("Minimum password length must be 6");
+                        flag=1;
+                    }
+                    if (newPassword.getText().length()<6) {
+                        newPassword.setError("Minimum password length must be 6");
+                        flag=1;
+                    }
+                    if (newPasswordConfirm.getText().length()<6) {
+                        newPasswordConfirm.setError("Minimum password length must be 6");
+                        flag=1;
+                    }
+                    if(oldPassword.getText().toString().equals("")  || oldPassword.getText().length()<6){
+                        oldPassword.setError("please enter your password");
+                        flag=1;
+                    }
+                    if(newPassword.getText().toString().equals("")){
+                        newPassword.setError("please enter your password");
+                        flag=1;
+                    }
+                    if(newPasswordConfirm.getText().toString().equals("")){
+                        newPasswordConfirm.setError("please enter your password");
+                        flag=1;
+                    }
+
+                    if (flag==0) {
+                        changePassword.setText("Change Password");
+                        changePassword.setBackgroundColor(Color.parseColor("#FFF44336"));
+                        linearChangePassword.setVisibility(View.GONE);
+                        linearShow.setVisibility(View.VISIBLE);
+                        ed.setVisibility(View.VISIBLE);
+                        DoctorMain.doctor.setPassword(newPassword.getText().toString());
+                        change();
+                        Toast.makeText(getActivity(),"Your Password is Changed",Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+                else {
+                    changePassword.setText("Submit");
+                    changePassword.setBackgroundColor(Color.parseColor("#FF4CAF50"));
+                    linearChangePassword.setVisibility(View.VISIBLE);
+                    linearShow.setVisibility(View.GONE);
+                    ed.setVisibility(View.GONE);
+                }
             }
         });
 
@@ -87,10 +197,27 @@ public class DoctorProfileFragment extends Fragment {
 
                 "name", nm.getText().toString(),
                 "username", un.getText().toString(),
-                "email", em.getText().toString(),
-                "password", pass.getText().toString()
-
+                "email", em.getText().toString()
         );
 
+    }
+
+    void change(){
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        db.collection("users").document("doctors")
+                .collection("data").document(doctor.getFileNumber() + "").update(
+                "password", newPassword.getText().toString()
+        );
+    }
+
+    void refresh(){
+        nm.setText(doctor.getName());
+        un.setText(doctor.getUsername());
+        em.setText(doctor.getEmail());
+
+        usernmaeTv.setText("Username : "+doctor.getUsername());
+        emailTv.setText("Email : "+doctor.getEmail());
+        fileNumberbTv.setText("File Number : "+doctor.getFileNumber());
+        nameTv.setText("Name : "+doctor.getName());
     }
 }
