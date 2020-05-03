93f49fada0fd116fe0dadc094e0251135aa991a0
hussein barakat <husseinbarakat678@gmail.com>
2020-05-03 14:47:37 +0300

    profile Design Student (Ended)

--- app/src/main/java/com/example/lababsencesystem/StudentProfileFragment.java
+++ app/src/main/java/com/example/lababsencesystem/StudentProfileFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,8 +39,8 @@ public class StudentProfileFragment extends Fragment {
 
     EditText nm,un,em,pass,fnb,oldPassword,newPassword,newPasswordConfirm;
     TextView usernmaeTv,fileNumberbTv,emailTv,nameTv;
-    Button ed,changePassword;
-    LinearLayout linearEdit,linearShow,linearChangePassword;
+    Button ed,changePassword,sb,sb2;
+    LinearLayout linearEC,linearLayoutC,la;
     int cur=0;
 
     public StudentProfileFragment() {
@@ -55,31 +57,23 @@ public class StudentProfileFragment extends Fragment {
 
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
-/*
+
+        fnb=view.findViewById(R.id.FNED);
         nm=view.findViewById(R.id.studentProfilename);
         un=view.findViewById(R.id.studentProfileUsername);
         em=view.findViewById(R.id.studentProfileEmail);
-        //pass=view.findViewById(R.id.password);
-        //fnb=view.findViewById(R.id.filenb);
-         ed = view.findViewById(R.id.editStudentProfile);
-         changePassword = view.findViewById(R.id.changePasswordSt);
-
-        usernmaeTv=view.findViewById(R.id.studentProfileUsernametv);
-        emailTv=view.findViewById(R.id.studentProfileEmailtv);
-        fileNumberbTv=view.findViewById(R.id.studentProfileFileNumberTv);
-        nameTv=view.findViewById(R.id.studentProfilenametv);
-
+        nameTv=view.findViewById(R.id.nameTvSt);
         oldPassword=view.findViewById(R.id.oldPasswordSt);
         newPassword=view.findViewById(R.id.newPasswordSt);
         newPasswordConfirm=view.findViewById(R.id.newPasswordConfirmSt);
 
-        linearChangePassword=view.findViewById(R.id.linearChangePasswordSt);
-        linearEdit=view.findViewById(R.id.linearEditSt);
-        linearShow=view.findViewById(R.id.linearShowSt);
-
-        linearShow.setVisibility(View.VISIBLE);
-        linearEdit.setVisibility(View.GONE);
-        linearChangePassword.setVisibility(View.GONE);
+        ed = view.findViewById(R.id.editStudentProfile);
+        changePassword = view.findViewById(R.id.changePasswordSt);
+        sb=view.findViewById(R.id.submitProfileSt);
+        linearEC=view.findViewById(R.id.layEC);
+        linearLayoutC=view.findViewById(R.id.laC);
+        la=view.findViewById(R.id.la);
+        sb2=view.findViewById(R.id.submit2);
 
         refresh();
 
@@ -88,30 +82,44 @@ public class StudentProfileFragment extends Fragment {
         ed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                nm.setCursorVisible(true);
+                nm.setFocusableInTouchMode(true);
+                un.setCursorVisible(true);
+                un.setFocusableInTouchMode(true);
+                em.setCursorVisible(true);
+                em.setFocusableInTouchMode(true);
+                nm.setInputType(InputType.TYPE_CLASS_TEXT);
+                un.setInputType(InputType.TYPE_CLASS_TEXT);
+                em.setInputType(InputType.TYPE_CLASS_TEXT);
 
-                String getEditPosition=ed.getText().toString();
-                if (getEditPosition.equalsIgnoreCase("Submit")){
-                    int flag=0;
-                    if (nm.getText().toString().equals("") ){
+                cur=1;
+                sb.setVisibility(View.VISIBLE);
+                linearEC.setVisibility(View.GONE);
+            }
+        });
+
+        sb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    int flag = 0;
+                    if (!isValidEmail(em.getText().toString())){
+                        em.setError("Invalid email format");
+                        flag = 1;
+                    }
+                    if (nm.getText().toString().equals("")) {
                         nm.setError("please enter your name");
-                        flag=1;
+                        flag = 1;
                     }
-                    if(em.getText().toString().equals("")) {
+                    if (em.getText().toString().equals("")) {
                         em.setError("please enter your email");
-                        flag=1;
+                        flag = 1;
                     }
-                    if(un.getText().toString().equals("")) {
+                    if (un.getText().toString().equals("")) {
                         un.setError("please enter your username");
-                        flag=1;
+                        flag = 1;
                     }
-                    if (flag==0) {
-                        ed.setText("Edit");
-                        //ed.setBackgroundColor(Color.parseColor("#FF3F51B5"));
-                        ed.setBackgroundResource(R.drawable.custom_button_2);
 
-                        linearShow.setVisibility(View.VISIBLE);
-                        linearEdit.setVisibility(View.GONE);
-                        changePassword.setVisibility(View.VISIBLE);
+                    if (flag == 0) {
                         StudentMain.student.setEmail(em.getText().toString());
                         StudentMain.student.setName(nm.getText().toString());
                         StudentMain.student.setUsername(un.getText().toString());
@@ -119,85 +127,68 @@ public class StudentProfileFragment extends Fragment {
                         refresh();
                     }
 
-                }
-                else {
-                    ed.setText("Submit");
-                    ed.setBackgroundResource(R.drawable.custom_button);
-                    //ed.setBackgroundColor(Color.parseColor("#FF4CAF50"));
-                    linearEdit.setVisibility(View.VISIBLE);
-                    linearShow.setVisibility(View.GONE);
-                    changePassword.setVisibility(View.GONE);
-                }
-
             }
-
-
         });
 
         changePassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String getPasswordPosition=changePassword.getText().toString();
-                if (getPasswordPosition.equalsIgnoreCase("Submit")){
-                    int flag=0;
+                linearEC.setVisibility(View.GONE);
+                sb2.setVisibility(View.VISIBLE);
+                linearLayoutC.setVisibility(View.VISIBLE);
+                la.setVisibility(View.GONE);
+            }
+        });
 
-                    if (!oldPassword.getText().toString().equals(student.getPassword())) {
-                        oldPassword.setError("Incorrect Password");
-                        flag = 1;
-                    }
-                    if (!newPassword.getText().toString().equals(newPasswordConfirm.getText().toString())){
-                        Toast.makeText(getActivity(),"new Password and Confirm new Password fields are different",Toast.LENGTH_LONG).show();
-                        flag=1;
-                    }
-                    if (oldPassword.getText().length()<6) {
-                        oldPassword.setError("Minimum password length must be 6");
-                        flag=1;
-                    }
-                    if (newPassword.getText().length()<6) {
-                        newPassword.setError("Minimum password length must be 6");
-                        flag=1;
-                    }
-                    if (newPasswordConfirm.getText().length()<6) {
-                        newPasswordConfirm.setError("Minimum password length must be 6");
-                        flag=1;
-                    }
-                    if(oldPassword.getText().toString().equals("")  || oldPassword.getText().length()<6){
-                        oldPassword.setError("please enter your password");
-                        flag=1;
-                    }
-                    if(newPassword.getText().toString().equals("")){
-                        newPassword.setError("please enter your password");
-                        flag=1;
-                    }
-                    if(newPasswordConfirm.getText().toString().equals("")){
-                        newPasswordConfirm.setError("please enter your password");
-                        flag=1;
-                    }
+        sb2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int flag = 0;
 
-                    if (flag==0) {
-                        changePassword.setText("Change Password");
-                        //changePassword.setBackgroundColor(Color.parseColor("#FFF44336"));
-                        changePassword.setBackgroundResource(R.drawable.custom_button_2);
-
-                        linearChangePassword.setVisibility(View.GONE);
-                        linearShow.setVisibility(View.VISIBLE);
-                        ed.setVisibility(View.VISIBLE);
-                        StudentMain.student.setPassword(newPassword.getText().toString());
-                        change();
-                        Toast.makeText(getActivity(),"Your Password is Changed",Toast.LENGTH_SHORT).show();
-
-                    }
-
+                if (!oldPassword.getText().toString().equals(student.getPassword())) {
+                    oldPassword.setError("Incorrect Password");
+                    flag = 1;
                 }
-                else {
-                    changePassword.setText("Submit");
-                    changePassword.setBackgroundResource(R.drawable.custom_button);
-                    linearChangePassword.setVisibility(View.VISIBLE);
-                    linearShow.setVisibility(View.GONE);
-                    ed.setVisibility(View.GONE);
+                if (!newPassword.getText().toString().equals(newPasswordConfirm.getText().toString())) {
+                    Toast.makeText(getActivity(), "new Password and Confirm new Password fields are different", Toast.LENGTH_LONG).show();
+                    flag = 1;
+                }
+                if (oldPassword.getText().length() < 6) {
+                    oldPassword.setError("Minimum password length must be 6");
+                    flag = 1;
+                }
+                if (newPassword.getText().length() < 6) {
+                    newPassword.setError("Minimum password length must be 6");
+                    flag = 1;
+                }
+                if (newPasswordConfirm.getText().length() < 6) {
+                    newPasswordConfirm.setError("Minimum password length must be 6");
+                    flag = 1;
+                }
+                if (oldPassword.getText().toString().equals("") || oldPassword.getText().length() < 6) {
+                    oldPassword.setError("please enter your password");
+                    flag = 1;
+                }
+                if (newPassword.getText().toString().equals("")) {
+                    newPassword.setError("please enter your password");
+                    flag = 1;
+                }
+                if (newPasswordConfirm.getText().toString().equals("")) {
+                    newPasswordConfirm.setError("please enter your password");
+                    flag = 1;
+                }
+                if (flag == 0) {
+                    StudentMain.student.setPassword(newPassword.getText().toString());
+                    change();
+                    Toast.makeText(getActivity(), "Your Password is Changed", Toast.LENGTH_SHORT).show();
+                    refresh();
                 }
             }
-        });*/
+        });
+
+
+
+
 
 
         return view;
@@ -243,14 +234,35 @@ public class StudentProfileFragment extends Fragment {
     }
 
     void refresh(){
+        fnb.setText(student.getFileNumber() +"");
         nm.setText(student.getName());
         un.setText(student.getUsername());
         em.setText(student.getEmail());
+        nameTv.setText(student.getName());
+        sb.setVisibility(View.GONE);
+        linearEC.setVisibility(View.VISIBLE);
+        sb2.setVisibility(View.GONE);
+        linearLayoutC.setVisibility(View.GONE);
+        la.setVisibility(View.VISIBLE);
 
-        usernmaeTv.setText("Username : "+student.getUsername());
-        emailTv.setText("Email : "+student.getEmail());
-        fileNumberbTv.setText("File Number : "+student.getFileNumber());
-        nameTv.setText("Name : "+student.getName());
+
+
+        nm.setInputType(InputType.TYPE_NULL);
+        un.setInputType(InputType.TYPE_NULL);
+        em.setInputType(InputType.TYPE_NULL);
+
+        nm.setFocusable(false);
+        un.setCursorVisible(false);
+        un.setFocusable(false);
+        un.setCursorVisible(false);
+        em.setFocusable(false);
+        em.setCursorVisible(false);
+
+
+    }
+
+    public  boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 
 }
