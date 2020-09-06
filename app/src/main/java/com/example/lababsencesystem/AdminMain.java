9ca4e22d317faf0929ff73120f8b26055f1e0eeb
hussein barakat <husseinbarakat678@gmail.com>
2020-09-06 12:33:26 +0300

    AdminAddDeleteDesign

--- app/src/main/java/com/example/lababsencesystem/AdminMain.java
+++ app/src/main/java/com/example/lababsencesystem/AdminMain.java
@@ -1,14 +1,52 @@
 package com.example.lababsencesystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class AdminMain extends AppCompatActivity {
+
+    TabLayout tabLayoutAdmin;
+    public ViewPager viewPager;
+    public ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_main);
+
+        tabLayoutAdmin=findViewById(R.id.tabLayoutAdmin);
+        viewPager=findViewById(R.id.PagerIdAdmin);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 1) {
+                    viewPager.getAdapter().notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new AdminAddDeleteFragment(), "Add / Delete");
+        viewPagerAdapter.addFragment(new AdminAssignCourseFragment(), "Assign Course");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPagerAdapter.notifyDataSetChanged();
+        tabLayoutAdmin.setupWithViewPager(viewPager);
+
     }
 }
