065c584f5fa8d1242ba21387f5d7d793bf7c5774
alikashmar8 <45448230+alikashmar8@users.noreply.github.com>
2020-09-09 13:09:06 +0200

    Remove attendance student

--- app/src/main/java/com/example/lababsencesystem/LabAttendance.java
+++ app/src/main/java/com/example/lababsencesystem/LabAttendance.java
@@ -1,18 +1,23 @@
 package com.example.lababsencesystem;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -25,8 +30,9 @@ public class LabAttendance extends AppCompatActivity {
     Lab lab;
     ListView studentsListView;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    ArrayList<String> students;
+    ArrayList<CourseStudent> students;
     TextView noAttendanceText;
+    ArrayAdapter<CourseStudent> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +46,37 @@ public class LabAttendance extends AppCompatActivity {
         noAttendanceText = findViewById(R.id.noAttendanceText);
         students = new ArrayList<>();
 
+        studentsListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog ad = new AlertDialog.Builder(LabAttendance.this)
+                        .setTitle("Attendance")
+                        .setMessage("Are you sure you want to remove " + students.get(position).getName() + " from attendance ?")
+                        .setPositiveButton("Remove", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                db.collection("labs").document(lab.getId()).collection("attendance").document(students.get(position).getFileNumber() + "").delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        adapter.remove(adapter.getItem(position));
+                                        adapter.notifyDataSetChanged();
+                                        studentsListView.setAdapter(adapter);
+                                        Toast.makeText(getApplicationContext(), "cStudent removed !", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create();
+                ad.show();
+                return false;
+            }
+        });
+
         db.collection("labs").document(lab.getId()).collection("attendance").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -47,12 +84,12 @@ public class LabAttendance extends AppCompatActivity {
                 for (DocumentSnapshot document : task.getResult()) {
                     CourseStudent s = document.toObject(CourseStudent.class);
                     Log.d("attstudnt", s.toString());
-                    students.add(s.toString());
+                    students.add(s);
                 }
                 if (!students.isEmpty()) {
                     noAttendanceText.setVisibility(View.GONE);
                     studentsListView.setVisibility(View.VISIBLE);
-                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.activity_listview, R.id.textView, students);
+                    adapter = new ArrayAdapter(getApplicationContext(), R.layout.activity_listview, R.id.textView, students);
                     studentsListView.setAdapter(adapter);
                 } else {
                     studentsListView.setVisibility(View.GONE);
